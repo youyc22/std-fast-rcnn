2dba275be76e46ec94f9dc1b8aa69a4fc3778246
youyc22 <youyc22@mails.tsinghua.edu.cn>
2024-11-13 21:15:44 +0800

    1

--- 111.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import torch
-import torch.nn.functional as F
-
-offsets=2*torch.ones(10, 4)
-GT_offsets=torch.ones(10, 4)
-batch_size=5
-a=F.smooth_l1_loss(offsets, GT_offsets, reduction='sum') * 1. / batch_size
-print(a)
